048e425aed2213d2921d65765cf9852177373d64
KwbCde <kwab822@gmail.com>
2025-11-20 02:34:26 -0500

    Fix model loading

--- src/model_loader.py
+++ src/model_loader.py
@@ -1,26 +1,16 @@
 import torch
 from torchvision import models
 import torch.nn as nn
-import os
-
-# Loads the EfficientNet-B0 model with custom classifier head.
 
 def load_model(weights_path: str, device: str = "cpu"):
-   
-    #Load base architecture
+    # Build architecture
     model = models.efficientnet_b0(weights=None)
-    
-    # Replace classifier head to match your 4 classes
     model.classifier[1] = nn.Linear(1280, 4)
-    
-    # Load state dict
-    state = torch.load(weights_path, map_location=device)
+
+    # Force CPU loading always (fixes Streamlit CUDA deserialization errors)
+    state = torch.load(weights_path, map_location="cpu")
     model.load_state_dict(state)
-    
-    # Set evaluation mode
+
     model.eval()
-    
-    # Move to device
     model.to(device)
-    
     return model
